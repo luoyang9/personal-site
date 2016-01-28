7b39b5034aa2aefe2f2eb79bce2397cd7b8b52da
Charlie Zhang <peidong97@yahoo.ca>
2016-01-28 00:41:43 -0500

    preloading, few style tweaks

--- js/main.js
+++ js/main.js
@@ -4,3 +4,28 @@ function goToHome()
 {
 	$("#trigger-home").trigger('click');
 }
+
+window.onload = function()
+{
+	var preload = new createjs.LoadQueue();
+	preload.on("complete", handleComplete, this);
+	preload.loadManifest([
+     {src:"img/charliezhang.png"},
+     {src:"img/dismahjam.png"},
+     {src:"img/getout.png"},
+     {src:"img/greenidentity.png"},
+     {src:"img/lectorial.png"},
+     {src:"img/myomusic.png"},
+     {src:"img/shooter.png"},
+     {src:"img/voyagr.png"},
+     {src:"partial-home.html"},
+     {src:"partial-about.html"},
+     {src:"partial-projects.html"}
+ 	]);
+
+ 	function handleComplete()
+ 	{
+ 		$(".nav-init")[0].click();
+ 	}
+
+}
